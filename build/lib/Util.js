d6083932b2f7d99b4f75c3896c2262cf24a2f7e1
= <LuukOriginal@gmail.com>
2022-09-27 19:01:38 +0200

    removed build from git

--- build/lib/Util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getApiURL = void 0;
-const getApiURL = (SchoolDomain) => {
-    return `https://${SchoolDomain}.zportal.nl/api/v3`;
-};
-exports.getApiURL = getApiURL;
